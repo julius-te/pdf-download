711465b6d1be6c0025b0b2097e226f889fff0af9
Julius Tenhaeff <julius.tenhaeff@rwth-aachen.de>
2023-10-07 13:48:03 +0200

    Create prototype

--- src/popup.ts
+++ src/popup.ts
@@ -2,8 +2,24 @@ const contentElement = document.querySelector(
   "#content"
 ) as HTMLParagraphElement;
 
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  if (tabs.length >= 1) {
+    const tabId = tabs[0].id;
+    if (tabId !== undefined && tabId !== chrome.tabs.TAB_ID_NONE) {
+      chrome.runtime.sendMessage({
+        type: "get-urls",
+        tabId: tabId,
+      });
+    }
+  }
+});
+
 chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === "incoming") {
-    contentElement.innerHTML += message.url + "<br>";
+  if (message.type === "update-urls") {
+    if (message.urls.length === 0) {
+      contentElement.textContent = "No PDFs found";
+    } else {
+      contentElement.innerHTML = message.urls.join("<br/>");
+    }
   }
 });
